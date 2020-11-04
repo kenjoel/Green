b83cf6f83dfbac74c8962e2e6fc5dd3f3d49c666
Ken-muigai <kenmuigai980@gmail.com>
2020-11-04 19:56:34 +0300

    lets go

---
+++ app/src/main/java/com/kenjoel/green/LoginActivity.java
@@ -0,0 +1,121 @@
+package com.kenjoel.green;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.lEmail) EditText lEmail;
+    @BindView(R.id.lPassword) EditText lPassword;
+    @BindView(R.id.bLogin) Button bLogin;
+    @BindView(R.id.signup) TextView navig;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        bLogin.setOnClickListener(this);
+        navig.setOnClickListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                final FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    public void onClick(View v){
+
+        if(v == bLogin){
+            String email = lEmail.getText().toString().trim();
+            String pass = lPassword.getText().toString().trim();
+
+            validEmail(email);
+            passWordValid(pass);
+
+
+            firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Sign_Up error ", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Welcome back",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+        if (v == navig){
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public boolean validEmail(String email){
+        if(email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    public boolean passWordValid(String password){
+        if(password.length() < 6){
+            lPassword.setError("Your password must be above 6");
+            return  false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+}
+
---
+++ app/src/main/java/com/kenjoel/green/SignUpActivity.java
@@ -0,0 +1,151 @@
+package com.kenjoel.green;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG =  "here is the value";
+    @BindView(R.id.launch) Button mLauncher;
+    @BindView(R.id.name) EditText mName;
+    @BindView(R.id.email) EditText mEmail;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.confirm) EditText mConfirm;
+    @BindView(R.id.radioGroup) RadioGroup mRadioGroup;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+    private RadioButton idHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mLauncher.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                final FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+                if (user != null) {
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int selected = mRadioGroup.getCheckedRadioButtonId();
+        idHolder =  findViewById(selected);
+        Log.d(TAG, idHolder.toString());
+
+        if(idHolder.getText() == null){
+            return;
+        }
+
+        final String name = mName.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+        String pass = mPassword.getText().toString().trim();
+        String confirm = mConfirm.getText().toString().trim();
+
+        validName(name);
+        validEmail(email);
+        passWordValid(pass, confirm);
+
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(SignUpActivity.this, "SignUp error ", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignUpActivity.this, "Welcome Sign Up Success", Toast.LENGTH_LONG).show();
+                    String userId = mAuth.getCurrentUser().getUid();
+                    DatabaseReference UserReference = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+                    Map userInfo = new HashMap();
+                    userInfo.put("name", name);
+                    userInfo.put("gender", idHolder.getText().toString());
+                    userInfo.put("profileImageUrl", "default");
+                    UserReference.setValue(userInfo);
+                }
+            }
+        });
+    }
+
+    public boolean validName(String mname){
+        if(mname.equals("")){
+            mName.setError("Please Enter a valid name");
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean passWordValid(String password, String confirmPass){
+        if(password.length() < 6){
+            mPassword.setError("Your password must be above 6");
+            return  false;
+        }else if(!password.equals(confirmPass)){
+            mPassword.setError("Your passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean validEmail(String email){
+        if(email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+}
